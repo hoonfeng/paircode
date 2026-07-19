90c086b89cb26eba7e0c3c7b503426504f88a43f
hfen <739731746@qq.com>
2026-07-19 08:48:22 +0800

    desktop: 使用 web-ui-minimal 替代完整 web-ui 包

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -17,9 +17,6 @@ func main() {
 
 	wd, _ := os.Getwd()
 	distDir := filepath.Join(wd, "cmd", "desktop", "web-ui-minimal", "dist")
-	if _, err := os.Stat(distDir); os.IsNotExist(err) {
-		distDir = filepath.Join(wd, "web-ui-minimal", "dist")
-	}
 	log.Printf("[Desktop] distDir: %s", distDir)
 
 	wv := webkit.NewWebView()
